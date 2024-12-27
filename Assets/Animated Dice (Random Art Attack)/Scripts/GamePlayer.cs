2829a25800dfd0906710308c4c745df743e7b8a4
October-zhuo <1151973750@qq.com>
2024-12-27 19:23:42 +0800

    init project

---
+++ Assets/Animated Dice (Random Art Attack)/Scripts/GamePlayer.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using FlutterUnityIntegration;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayer : MonoBehaviour
+{
+    public GameObject user;
+    public GameObject another;
+    public RollJump rollJump;
+    public GameObject textView;
+    
+    private int userValue = 0;
+    private int anotherValue = 0;
+    private Text textComponent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = GameObject.Find("User");
+        another = GameObject.Find("Another");
+        rollJump = gameObject.GetComponent<RollJump>();
+        textView = GameObject.Find("ShowText");
+        if (user != null)
+        {
+            Debug.Log("拿到了user");
+        }
+        else
+        {
+            Debug.Log("无法获取user");
+        }
+        if (textView != null)
+        {
+            textComponent = textView.GetComponent<Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = "You hold the black dice.\nPress anywhere to roll!";
+            }
+            else
+            {
+                Debug.LogError("Text component not found on ShowText.");
+            }
+        }
+        else
+        {
+            Debug.LogError("GameObject ShowText not found in the scene.");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rollJump.onceFinished)
+        {
+            userValue = user.GetComponent<DiceStats>().side;
+            anotherValue = another.GetComponent<DiceStats>().side;
+            
+            if (userValue > anotherValue)
+            {
+                textComponent.text = "You win!";
+                StartCoroutine(TimerCoroutine("Successful"));
+            }
+            else if (userValue < anotherValue)
+            {  
+                textComponent.text = "You lose!";
+                StartCoroutine(TimerCoroutine("Failed"));
+                UnityMessageManager.Instance.SendMessageToFlutter("Failed");
+            }
+            else
+            {
+                textComponent.text = "Try Again!";
+                //textView.text = "Try Again!";
+            }
+            rollJump.onceFinished = false;
+        }
+    }
+    
+    private IEnumerator TimerCoroutine(string message)
+    {
+        yield return new WaitForSeconds(1);
+        UnityMessageManager.Instance.SendMessageToFlutter("Successful");
+    }
+}
+
---
+++ Assets/Animated Dice (Random Art Attack)/Scripts/RollJump.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Timers;
+using UnityEngine;
+using Timer = System.Timers.Timer;
+
+public class RollJump : MonoBehaviour
+{
+    // This list shows what dice are going to be and can be edited via code if you want or in the inspector. 
+    [SerializeField] List<GameObject> diceGroup = new List<GameObject>();
+    //Set what button is pressed to make the dice jump.
+    [SerializeField] float forceAmount = 400f;
+
+    public bool onceFinished = false;
+    
+    
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpBehavior();
+    }
+    void JumpBehavior()
+    {
+        Rigidbody rb;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            onceFinished = false;
+            StartCoroutine(TimerCoroutine());
+            for (int i = 0; i < diceGroup.Count; i++)
+            {
+                diceGroup[i].transform.rotation = Random.rotation;
+                rb = diceGroup[i].GetComponent<Rigidbody>();
+                // all the dices can not roll out of the screen
+                Vector3 position = diceGroup[i].transform.position;
+                if (position.y < 0) // 检查骰子是否在屏幕下方
+                {
+                    position.y = 0; // 将骰子位置重置到屏幕内
+                }
+                if (position.x < -Screen.width / 2 + 100 || position.x > Screen.width / 2 - 100)
+                {
+                    position.x = 0; // 将骰子位置重置到屏幕内
+                }
+                rb.AddForce(Vector3.up * forceAmount);
+                rb.AddTorque(new Vector3(Random.value * forceAmount, Random.value * forceAmount, Random.value * forceAmount));
+            }
+        }
+    }
+    
+    private IEnumerator TimerCoroutine()
+    {
+        yield return new WaitForSeconds(4);
+        onceFinished = true; 
+        yield return new WaitForSeconds(1);
+        onceFinished = false; 
+    }
+    
+}
+   
